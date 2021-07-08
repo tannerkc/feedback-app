26acb2c9ce1e8ee95b3fc20659cee39985a9bbfc
Tanner Cottle <tcottle8@gmail.com>
2021-07-07 19:13:04 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Dashboard from './pages/Dashboard';
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
+var feedbackContext = React.createContext(null);
 
 function App() {
+
+  const [feedbackData, setFeedbackData] = useState([])
+
+  useEffect(()=>{
+    axios.get('data.json').then(()=>{
+      
+    })
+  }, [])
+
   return (
     <div className="App">
+      <feedbackContext.Provider value={feedbackData}>
           <Dashboard />
+      </feedbackContext.Provider>
     </div>
   );
 }
