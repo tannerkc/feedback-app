17c122bd5e06cbb83707af341bb8c66a66592163
Tanner Cottle <tcottle8@gmail.com>
2021-07-07 22:32:38 -0500

    init

--- src/pages/FeedbackDetails.jsx
+++ src/pages/FeedbackDetails.jsx
@@ -8,7 +8,14 @@ export default function FeedbackDetails(props) {
 
     const feedbackObj = feedbackData.find(obj => obj.id == feedbackId);
 
+    // const [text, setText] = useState('')
+
     console.log(feedbackObj)
+
+    // const inputHandler = (text) =>{
+    //     setText(text)
+
+    // }
 
     return (
         <div>
@@ -16,6 +23,11 @@ export default function FeedbackDetails(props) {
                 feedbackObj.comments.map(comment=>(
                     <div className="commentCard" style={{margin: 30}}>
                         {comment.content}
+                        {/* <input type="text" 
+                        value={text}
+                        onChange={(e)=>{
+                            setText(e.target.value)
+                        }} /> */}
                     </div>
                 ))
             }
